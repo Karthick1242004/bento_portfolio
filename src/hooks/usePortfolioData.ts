68e19950cffac6f605342621537cbe195a8b6948
karthickrajan S <93571352+Karthick1242004@users.noreply.github.com>
2024-12-23 11:48:21 +0530

    Updated Gist URL in usePortfolioData.ts

--- src/hooks/usePortfolioData.ts
+++ src/hooks/usePortfolioData.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-const DATA_URL = 'https://gist.githubusercontent.com/Karthick1242004/c24e526349787ace0120ecb3163a6f98/raw/data.json';
+const DATA_URL = 'https://gist.githubusercontent.com/Karthick1242004/e3489e639d7a9aa5ac7d3d0bce25a872/raw/data.json';
 
 
 export function usePortfolioData() {
